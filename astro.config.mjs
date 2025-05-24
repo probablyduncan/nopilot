2c17a037219234f04fc73a6cdf9dd9409536b1c6
probablyduncan <duncanpetrie1@gmail.com>
2025-05-24 12:18:25 +0100

    netlify/mdx/etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: netlify(),
+  integrations: [mdx()]
+});
