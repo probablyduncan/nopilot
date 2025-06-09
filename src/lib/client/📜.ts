009501543e27804d78bac7bd503b1bafe57aae80
probablyduncan <duncanpetrie1@gmail.com>
2025-06-09 21:58:15 +0100

    ok some work

--- src/lib/client/📜.ts
+++ src/lib/client/📜.ts
@@ -6,5 +6,17 @@ export async function submitQuery(formData: FormData): Promise<void> {
     });
 
     const result = response.status === 200 ? await response.json() : { html: `Yeah, ok. Sure.` };
-    document.querySelectorAll(".response-wrapper").forEach(_e => { _e.innerHTML = result.html });
+    document.querySelectorAll(".response").forEach(_e => { _e.innerHTML = result.html });
+    toggleView(true);
+}
+
+export function toggleView(showResponse: boolean | undefined) {
+
+    const responseWrapper = (document.querySelector(".response-wrapper") as HTMLElement);
+    const startContent = (document.querySelector(".start-content") as HTMLElement);
+
+    showResponse ??= responseWrapper.hidden;
+
+    responseWrapper.hidden = !showResponse;
+    startContent.hidden = showResponse;
 }
